46018db519366382a3acebbb664b5fae32bf9228
Nyrek2001 <Nyrekwheeler@gmail.com>
2025-04-10 22:33:23 -0400

    first commit

---
+++ target/debug/deps/maam_project-1c7bb6ebd196b8a0.d
@@ -0,0 +1,5 @@
+C:\Users\mcelr\maam_project\target\debug\deps\maam_project-1c7bb6ebd196b8a0.exe: src\lib.rs
+
+C:\Users\mcelr\maam_project\target\debug\deps\maam_project-1c7bb6ebd196b8a0.d: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/maam_project-29204fafc6e3a703.d
@@ -0,0 +1,7 @@
+C:\Users\mcelr\maam_project\target\debug\deps\libmaam_project-29204fafc6e3a703.rmeta: src\lib.rs
+
+C:\Users\mcelr\maam_project\target\debug\deps\libmaam_project-29204fafc6e3a703.rlib: src\lib.rs
+
+C:\Users\mcelr\maam_project\target\debug\deps\maam_project-29204fafc6e3a703.d: src\lib.rs
+
+src\lib.rs:
